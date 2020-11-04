d5d3951b2d5ba8a96720c37f4423e7c6ff413176
Brandon Wong <brandon@yerdle.com>
2020-11-04 13:40:35 -0600

    Implement bubble sort

---
+++ src/com/blhwong/bubblesort/Program.java
@@ -0,0 +1,17 @@
+package com.blhwong.bubblesort;
+
+class Program {
+
+    public static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+}
