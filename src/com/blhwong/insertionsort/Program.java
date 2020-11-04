24f157cac5d380ffd82caf7b78472f5b903e71b9
Brandon Wong <brandon@yerdle.com>
2020-11-04 13:51:30 -0600

    Implement insertion sort

---
+++ src/com/blhwong/insertionsort/Program.java
@@ -0,0 +1,18 @@
+package com.blhwong.insertionsort;
+
+
+class Program {
+
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+}
