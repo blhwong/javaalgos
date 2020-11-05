16f8fab67149cc72c3d84c3d754ac20c806ce307
Brandon Wong <brandon@yerdle.com>
2020-11-05 15:13:50 -0600

    Implement move element to end

---
+++ src/com/blhwong/moveelementtoend/Program.java
@@ -0,0 +1,24 @@
+package com.blhwong.moveelementtoend;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Program {
+
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        int left = 0;
+        int right = array.size() - 1;
+        while (left < right) {
+            while (left < right && array.get(right) == toMove) {
+                right--;
+            }
+            if (array.get(left) == toMove) {
+                Collections.swap(array, left, right);
+            }
+            left++;
+        }
+        return array;
+    }
+}
---
+++ test/com/blhwong/moveelementtoend/ProgramTest.java
@@ -0,0 +1,27 @@
+package com.blhwong.moveelementtoend;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProgramTest {
+
+    @Test
+    public void TestCase1() {
+        List<Integer> array = new ArrayList<Integer>(Arrays.asList(2, 1, 2, 2, 2, 3, 4, 2));
+        int toMove = 2;
+        List<Integer> expectedStart = new ArrayList<Integer>(Arrays.asList(1, 3, 4));
+        List<Integer> expectedEnd = new ArrayList<Integer>(Arrays.asList(2, 2, 2, 2, 2));
+        List<Integer> output = Program.moveElementToEnd(array, toMove);
+        List<Integer> outputStart = output.subList(0, 3);
+        outputStart.sort(Comparator.naturalOrder());
+        List<Integer> outputEnd = output.subList(3, output.size());
+        assertTrue(outputStart.equals(expectedStart));
+        assertTrue(outputEnd.equals(expectedEnd));
+    }
+}
