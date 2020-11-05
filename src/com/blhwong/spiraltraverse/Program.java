4d633ac03519d4d57ff286e578eab673b1eff421
Brandon Wong <brandon@yerdle.com>
2020-11-05 15:45:15 -0600

    Implement spiral matrix

---
+++ src/com/blhwong/spiraltraverse/Program.java
@@ -0,0 +1,45 @@
+package com.blhwong.spiraltraverse;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Program {
+
+    public static List<Integer> spiralTraverse(int[][] array) {
+        int left = 0;
+        int right = array[0].length - 1;
+        int up = 0;
+        int down = array.length - 1;
+        List<Integer> ans = new ArrayList<>();
+        int size = array.length * array[0].length;
+        while (ans.size() < size) {
+            for (int j = left; j <= right; j++) {
+                if (ans.size() < size) {
+                    ans.add(array[up][j]);
+                }
+            }
+            for (int i = up + 1; i <= down; i++) {
+                if (ans.size() < size) {
+                    ans.add(array[i][right]);
+                }
+            }
+            for (int j = right - 1; j >= left; j--) {
+                if (ans.size() < size) {
+                    ans.add(array[down][j]);
+                }
+            }
+            for (int i = down - 1; i > up; i--) {
+                if (ans.size() < size) {
+                    ans.add(array[i][left]);
+                }
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+        return ans;
+    }
+}
---
+++ test/com/blhwong/spiraltraverse/ProgramTest.java
@@ -0,0 +1,23 @@
+package com.blhwong.spiraltraverse;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProgramTest {
+
+    @Test
+    public void TestCase1() {
+        var input = new int[][] {
+                        {1, 2, 3, 4},
+                        {12, 13, 14, 5},
+                        {11, 16, 15, 6},
+                        {10, 9, 8, 7},
+                };
+        var expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        var actual = Program.spiralTraverse(input);
+        assertTrue(expected.equals(actual));
+    }
+}
