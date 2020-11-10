c2e3a96637530699264c44fa5e0774690e07de20
Brandon Wong <brandon@yerdle.com>
2020-11-10 00:35:04 -0600

    Implement invert binary tree

---
+++ src/com/blhwong/invertbinarytree/Program.java
@@ -0,0 +1,25 @@
+package com.blhwong.invertbinarytree;
+
+class Program {
+
+    public static void invertBinaryTree(BinaryTree tree) {
+        if (tree == null) {
+            return;
+        }
+        BinaryTree tmp = tree.left;
+        tree.left = tree.right;
+        tree.right = tmp;
+        invertBinaryTree(tree.left);
+        invertBinaryTree(tree.right);
+    }
+
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
